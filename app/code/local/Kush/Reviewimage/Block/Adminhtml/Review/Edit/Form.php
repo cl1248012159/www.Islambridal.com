a46027422e1579270dcf1f0d7c3cdb31f51ba598
leichen <313399524@qq.com>
2016-07-16 16:33:47 +0800

    review

--- app/code/local/Kush/Reviewimage/Block/Adminhtml/Review/Edit/Form.php
+++ app/code/local/Kush/Reviewimage/Block/Adminhtml/Review/Edit/Form.php
@@ -81,26 +81,26 @@ class Kush_Reviewimage_Block_Adminhtml_Review_Edit_Form extends Mage_Adminhtml_B
            $image = '';
            if($review->getReviewimage1()){
                $imageUrl = Mage::getBaseUrl("media").'reviewimages/'.$review->getReviewimage1();
-               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;'>";
+               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;margin: 0 5px;'>";
            }
            if($review->getReviewimage2()){
                $imageUrl = Mage::getBaseUrl("media").'reviewimages/'.$review->getReviewimage2();
-               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;'>";
+               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;margin: 0 5px;'>";
 
            }
            if($review->getReviewimage3()){
                $imageUrl = Mage::getBaseUrl("media").'reviewimages/'.$review->getReviewimage3();
-               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;'>";
+               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;margin: 0 5px;'>";
 
            }
            if($review->getReviewimage4()){
                $imageUrl = Mage::getBaseUrl("media").'reviewimages/'.$review->getReviewimage4();
-               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;'>";
+               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;margin: 0 5px;'>";
 
            }
            if($review->getReviewimage5()){
                $imageUrl = Mage::getBaseUrl("media").'reviewimages/'.$review->getReviewimage5();
-               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;'>";
+               $image .= "<image src='".$imageUrl."' style='max-width:200px;max-height:200px;margin: 0 5px;'>";
            }
            $fieldset->addField('reviewimages', 'note', array(
                'label'     => Mage::helper('review')->__('Posted Review Image'),
