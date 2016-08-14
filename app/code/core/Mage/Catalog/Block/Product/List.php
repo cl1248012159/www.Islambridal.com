4f84493f1449e247e8e6ad23628a3bd7d7a655e2
leichen <313399524@qq.com>
2016-08-14 18:17:22 +0800

    a

--- app/code/core/Mage/Catalog/Block/Product/List.php
+++ app/code/core/Mage/Catalog/Block/Product/List.php
@@ -139,7 +139,10 @@ class Mage_Catalog_Block_Product_List extends Mage_Catalog_Block_Product_Abstrac
         $toolbar = $this->getToolbarBlock();
 
         // called prepare sortable parameters
-        $collection = $this->_getProductCollection();
+        $collection = $this->_getProductCollection()
+            ->addAttributeToSort('updated_at', 'desc')
+            ->addAttributeToSort('entity_id', 'desc');
+
 
         // use sortable parameters
         if ($orders = $this->getAvailableOrders()) {
@@ -162,9 +165,7 @@ class Mage_Catalog_Block_Product_List extends Mage_Catalog_Block_Product_Abstrac
         Mage::dispatchEvent('catalog_block_product_list_collection', array(
             'collection' => $this->_getProductCollection()
         ));
-        $this->_getProductCollection()
-            ->addAttributeToSort('updated_at', 'desc')
-            ->addAttributeToSort('entity_id', 'desc');
+
         $this->_getProductCollection()->load();
 
 
